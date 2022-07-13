fa44ff0588ae701cd140d2e8a5c829d57f7cb2a3
Alejandro Perez Pujante <perezpujantealejandro1@gmail.com>
2022-07-13 18:14:50 +0200

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import { Heading } from '@chakra-ui/react'
+import type { NextPage } from 'next'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Heading>Howdy!</Heading>
+    </>
+  )
+}
+
+export default Home
